632a52c44d0166c29b0edced90f0a8da0b69541d
Muhammadkul-Khurboyev <154499804+Muhammadkul-Khurboyev@users.noreply.github.com>
2024-06-21 09:39:59 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var firstName = prompt("Ismingiz nima?");
+var lastName = prompt("Familiyangiz nima?");
+var fullName = prompt("Otangizni ismini kiriting!");
+var nationality = prompt("Millatingiz?","uzbek");
+var birthday = prompt("Tugilgan yil/oy/kuningizni kiriting!");
+var yourEmail = prompt("Emailingizni kiriting","muhammadqulxurboyev@gamil.com");
+var married = confirm("Uylangan yokida turmushga chiqqanmisiz");
+var occupation = prompt("KAsbingizni kiriting?");
+var yourCountry = prompt("Qaysi davlatda yashaysiz?","Uzbekiston");
+var yourCity = prompt("Yashash manzilingizni kiriting");
+
+alert("Malumotlar Korib chiqilmoqda iltimos 12000 yil kuting!!!!")-->
+
+
+console.log("foydalanuvchining ismi -->"+ firstName+"\n"+"Foydalanuvchining familiyasi -->"+lastName+"\n"+"Foydalanuvchining sharifi -->"+fullName+"\n"+"Foydalanuvchining milllati -->"+nationality+"\n"+"Foydalanuvchining tugilgan kuni -->"+birthday+"\n"+"Foydalanuvchining email adresi -->"+yourEmail+"\n"+"Oilaviy xolati -->"+married+"\n"+"Foydalanuvchining kasbi -->"+occupation+"\n"+"Foydalanuvchining davlati -->"+yourCountry+"\n"+"Foydalanuvchining yashash joyi -->"+yourCity);
